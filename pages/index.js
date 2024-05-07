32e30454c091251174e21435557f24cb8af465e5
karimaljundi <jundiak@gmail.com>
2024-05-07 00:05:53 -0400

    added album covers to the home page if the user is signed in

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,12 @@ import { useEffect, useState } from 'react';
 import DropDown from '@/components/DropDown'
 import Card from '@/components/Card';
 
+
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [albums, setAlbums] = useState([]);
   const {data:session} = useSession();
   const [x, setX] = useState('');
   const [profile, setProfile] = useState([]);
@@ -31,17 +34,50 @@ export default function Home() {
     }
     fetchData()}, [session]);
     
+
+    useEffect(()=> {
+      async function fetchData() {
+          if (session && session.accessToken){
+            const offset = 0; // Start from the beginning
+            const limit = 50; // Limit to 200 results per page
+            
+            const response = await fetch(`https://api.spotify.com/v1/search?q=%25a%25&type=album&limit=${limit}&offset=${offset}`, {
+              headers: {
+                Authorization: `Bearer ${session.accessToken}`
+              }
+            });
+            const data = await response.json();
+              console.log(data);
+              setAlbums(data.albums.items);
+            }
+           
+      } fetchData();}, [session])
+    const topAlbumList = albums?.map((item) => {
+      return (
+        <div className='opacity-50' key={item.id}>
+          <img src={item.images[0].url} alt={item.name} />
+        </div>
+      )
+    })
   
   return (
     <div>
-      <Navbar />
-      <main className={`flex min-h screen flex-col items-center
-       justify-between p-24 ${inter.className}`}>
-        <div>Stats for Spotify Profile </div>
-     <div>
-      {profile.display_name}
+      
+      < main className={`${inter.className}`}>
+      <div className='grid  grid-cols-10 grid-rows-3' >
+{topAlbumList}
+      
      </div>
+     <div className='absolute inset-x-0 top-0 h-16 bg-opacity-0'>
+     <Navbar />
+     <div>Stats for Spotify Profile </div>
+
+
+     </div>
+      
+     
        </main>
+      
     </div>
   )
 }
